2dea188df59ce0f69bbf536c324036082c3a9d5e
simon <simon@frostick.io>
2018-07-20 16:12:18 +0100

    initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'notepadplusplus'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'npp.7.5.7.Installer.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  file         = $fileLocation
+
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
